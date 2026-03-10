9ef8d4feeebaef064d7bddff8049825be0858ff8
Sebastian Plaza <sebaplaza@users.noreply.github.com>
2026-03-10 17:55:23 +0100

    chore: fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/birdi/" : "/",
   test: {
     exclude: ["e2e/**", "node_modules/**"],
   },
